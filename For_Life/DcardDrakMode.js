2bfe044849d8e68e4464c7c3928aeb4e4cfd0de3
Ethan <Jason_coder@protonmail.com>
2024-09-15 05:13:31 +0900

    2024.09.15

--- For_Life/DcardDrakMode.js
+++ For_Life/DcardDrakMode.js
@@ -10,44 +10,49 @@
 // ==/UserScript==
 
 (function() {
-  'use strict';
+    'use strict';
 
-  const css = `
-      body, html {
-          background-color: #181817 !important;
-          color: #d3d3d3 !important;
-      }
-      * :not(em):not(.d_gy_1x2esgu):not(.d_34_2q):not(.d_5s_28):not(.d_nf_1y) {
-          background-color: inherit !important;
-          color: inherit !important;
-          border-color: #181817 !important;
-      }
-      a {
-          color: #d3d3d3 !important;
-      }
-      input, textarea, select, button {
-          background-color: #181618 !important;
-          color: #d3d3d3 !important;
-          border: 1px solid #181817 !important;
-      }
-      input::placeholder, textarea::placeholder {
-          color: #a9a9a9 !important;
-      }
-      .d_nf_1y {
-          --1m8iehv: inherit !important;
-      }
-      .pf9i80d::after {
-          background-color: #3397CF !important;
-      }
-      img {
-          filter: brightness(0.7) contrast(1.2) !important;
-      }
-      .d_xe_3b.d_ei_3b.d_9w_25.d_an_8.d_10fpds0_1j.epy1cme {
-          fill: #d3d3d3 !important;
-      }
-  `;
+    const css = `
+        body, html {
+            background-color: #181817 !important;
+            color: #d3d3d3 !important;
+            --color-text-secondary: rgba(211, 211, 211, 0.1);
+        }
+        * :not(em):not(.d_gy_1x2esgu):not(.d_34_2q):not(.d_5s_28):not(.d_nf_1y):not(.dx53m09) {
+            background-color: inherit !important;
+            color: inherit !important;
+            border-color: #181817 !important;
+        }
+        a {
+            color: #d3d3d3 !important;
+        }
+        input, textarea, select, button {
+            background-color: #181618 !important;
+            color: #d3d3d3 !important;
+            border: 1px solid #181817 !important;
+        }
+        input::placeholder, textarea::placeholder {
+            color: #a9a9a9 !important;
+        }
+        img {
+            filter: brightness(0.7) contrast(1.2) !important;
+        }
+        .pf9i80d::after {
+            background-color: #3397CF !important;
+        }
+        .d_nf_1y, .rwqnao9, .lm8hbrk {
+            --1m8iehv: inherit !important;
+        }
+        .d_xf_3b.d_ej_3b.d_9w_25.d_ao_8.d_10fpds0_1j.epy1cme.SVGIcon-module__er3F7q__svgIcon {
+            fill: #d3d3d3 !important;
+        }
+        .p25mgcu {
+            display: none;
+        }
+    `;
 
-  const style = document.createElement('style');
-  style.appendChild(document.createTextNode(css));
-  document.head.appendChild(style);
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.appendChild(document.createTextNode(css));
+    document.head.appendChild(style);
 })();
